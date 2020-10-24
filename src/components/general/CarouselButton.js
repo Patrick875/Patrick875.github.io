b2ea2a387e26f4ae8ab912fd0dc7e4852183d8de
Patrick875 <kubwimanapatrick@gmail.com>
2020-10-24 06:35:07 +0200

    Updates

--- src/components/general/CarouselButton.js
+++
@@ -1,8 +0,0 @@
-//jshint esversion:9
-import React from "react";
-
-function CarouselButton(props) {
-	return <button>{props.children}</button>;
-}
-
-export default CarouselButton;
--- src/components/general/Resources_download_btn.js
+++
@@ -1,8 +0,0 @@
-//jshint esversion:9
-import React from "react";
-
-function ResourcesDownloadBtn(props) {
-	return <button className={props.class}>{props.children}</button>;
-}
-
-export default ResourcesDownloadBtn;
